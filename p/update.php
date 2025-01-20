43962b4c5f509171052ee37ff510ede595cb073c
a991e <pingkang9021fyc@163.com>
2025-01-20 21:04:41 +0800

    check token

--- p/update.php
+++ p/update.php
@@ -1,7 +1,13 @@
 <?php
+require(__DIR__ . '/../constants.php');
+require(LIB_PATH . '/lib_rss.php');
+
 if (isset($_POST["token"])) {
-        $token = $_POST["token"];
-        if ($token === getenv("TOKEN")) {
+        FreshRSS_Context::initSystem();
+        $user = FreshRSS_Context::systemConf()->default_user;
+        FreshRSS_Context::initUser($user);
+        $token = FreshRSS_Context::userConf()->token;
+        if ($token === $_POST["token"]) {
             $command = "php " . __DIR__ . "/../app/actualize_script.php > /tmp/FreshRSS.log 2>&1 &";
             pclose(popen($command,'r'));
             die('1');
