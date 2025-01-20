044f609c3ce05dbd6afe824d6ee7f18e8fb39ce8
a991e <pingkang9021fyc@163.com>
2025-01-20 16:39:54 +0800

    Update update.php

--- p/update.php
+++ p/update.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["token"])) {
         $token = $_POST["token"];
-        if ($token === "") {
+        if ($token === $_SERVER["TOKEN"]) {
             $command = "php " . __DIR__ . "/../app/actualize_script.php > /tmp/FreshRSS.log 2>&1 &";
             pclose(popen($command,'r'));
             die('1');
